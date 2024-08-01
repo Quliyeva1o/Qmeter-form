643ffa2fb72e1b921e2b2ff100c3e69ee84f5f28
Quliyeva1o <renaquliyeva646@gmail.com>
2024-08-01 15:42:39 +0400

    form mapped

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,13 +4,23 @@ import TextField from '@mui/material/TextField';
 import { Select } from 'antd';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import { get } from '../API/requests.js'
+import { get } from '../API/requests.js';
 import { AsYouType } from 'libphonenumber-js';
 import uss from '../assets/us.png';
 import countryOptions, { LocalCountries } from '../utils/constants.tsx';
 
-
-
+const validationSchema = yup.object({
+    country: yup.string().required('Country is required'),
+    name: yup.string()
+        .required('Name is required')
+        .test('word-count', 'Name must be 2 or 3 words', (value) => {
+            if (!value) return false;
+            const wordCount = value.trim().split(/\s+/).length;
+            return wordCount === 2 || wordCount === 3;
+        }),
+    phone: yup.string().required('Phone number is required'),
+    email: yup.string().email('Invalid email format').required('Email is required'),
+});
 
 const Form = () => {
     const [selectedCountry, setSelectedCountry] = useState(null);
@@ -21,8 +31,13 @@ const Form = () => {
             try {
                 const res = await get();
                 if (res.data && res.data.country) {
+                    const country = countryOptions.find((x) => x.localeName === res.data.country);
                     setLocalCountry(res.data.country);
-                    setSelectedCountry(countryOptions.find((x)=>x.localeName==res.data.country) )
+                    setSelectedCountry(country);
+                    if (country) {
+                        formik.setFieldValue('num', country.code);
+                        formik.setFieldValue('country', country.country);
+                    }
                 }
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -31,42 +46,82 @@ const Form = () => {
 
         fetchData();
     }, []);
-    console.log(selectedCountry);
-
-
-    const validationSchema = yup.object({
-        country: yup.string().required('Country is required'),
-        name: yup.string().required('Name is required'),
-        phone: yup.string().required('Phone number is required'),
-        email: yup.string().email('Invalid email format').required('Email is required'),
-    });
-
 
     const formik = useFormik({
         initialValues: {
-            country: selectedCountry && selectedCountry.country,
+            num: '', // Initial value for country code
+            country: '',
             name: '',
             phone: '',
             email: '',
         },
         validationSchema,
         onSubmit: values => {
+            const formattedNumber = new AsYouType(values.num.replace('+', '')).input(values.phone);
+            console.log({
+                ...values,
+                phone: formattedNumber
+            });
             formik.resetForm();
-            console.log(values);
         },
     });
 
     const handlePhoneChange = (e) => {
-        const phoneNumber = e.target.value;
-        const formattedNumber = new AsYouType(selectedCountry && selectedCountry.code.replace('+', '')).input(phoneNumber);
-        formik.setFieldValue('phone', formattedNumber);
+        formik.setFieldValue('phone', e.target.value);
     };
 
     const handleCountryChange = (value) => {
         const selectedOption = countryOptions.find(option => option.code === value);
         if (selectedOption) {
-            setSelectedCountry(selectedOption);
             formik.setFieldValue('country', selectedOption.country);
+        }
+    };
+
+    const renderField = (field) => {
+        switch (field.name) {
+            case 'phone':
+                return (
+                    <CountryPhone
+                        formik={formik}
+                        value={formik.values.num}
+                        phonechange={handlePhoneChange}
+                    />
+                );
+            case 'country':
+                return (
+                    <Select
+                        placeholder={field.placeholder}
+                        fullWidth
+                        id={field.name}
+                        name={field.name}
+                        className={formik.touched[field.name] && formik.errors[field.name] ? styles.error : styles.selectt}
+                        value={formik.values.country} 
+                        onChange={handleCountryChange}
+                        onBlur={formik.handleBlur}
+                        options={countryOptions.map(option => ({
+                            value: option.code,
+                            label: (
+                                <div className={styles.optionContent}>
+                                    <p>{option.country}</p>
+                                </div>
+                            ),
+                        }))}
+                    />
+                );
+            default:
+                return (
+                    <TextField
+                        placeholder={field.placeholder}
+                        fullWidth
+                        id={field.name}
+                        name={field.name}
+                        className={formik.touched[field.name] && formik.errors[field.name] ? styles.error : ""}
+                        value={formik.values[field.name]}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched[field.name] && Boolean(formik.errors[field.name])}
+                    />
+                );
         }
     };
 
@@ -90,82 +145,12 @@ const Form = () => {
                     <div className={styles.formDiv}>
                         <img src={LocalCountries[localCountry] || uss} alt="Country Flag" />
                         <form onSubmit={formik.handleSubmit}>
-                            <label htmlFor="name">Name</label>
-                            <TextField
-                                placeholder='Pietro Schirano'
-                                fullWidth
-                                id="name"
-                                name="name"
-                                value={formik.values.name}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                className={formik.touched.name && formik.errors.name ? styles.error : ""}
-                                error={formik.touched.name && Boolean(formik.errors.name)}
-                            />
-                            <label htmlFor="email">Email</label>
-                            <TextField
-                                placeholder='example@qmeter.net'
-                                fullWidth
-                                id="email"
-                                name="email"
-                                className={formik.touched.email && formik.errors.email ? styles.error : ""}
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                error={formik.touched.email && Boolean(formik.errors.email)}
-                            />
-
-
-                            <label htmlFor="phone">Phone number</label>
-                            <div
-                                className={formik.touched.phone && formik.errors.phone ? styles.error : styles.phoneNumber}
-                            >
-                                <Select
-                                    className={styles.phoneSelect}
-                                    value={selectedCountry && selectedCountry.code}
-                                    onChange={handleCountryChange}
-                                    style={{ width: '140px' }}
-                                    options={countryOptions.map(option => ({
-                                        value: option.code,
-                                        label: (
-                                            <div className={styles.optionContent}>
-                                                <img src={option.flag} alt={option.country} />
-                                                <p>{option.code}</p>
-                                            </div>
-                                        ),
-                                    }))}
-                                />
-
-                                <TextField
-                                    placeholder='(__) ___-__-__'
-                                    fullWidth
-                                    id="phone"
-                                    name="phone"
-                                    value={formik.values.phone}
-                                    onChange={handlePhoneChange}
-                                    onBlur={formik.handleBlur}
-                                    error={formik.touched.phone && Boolean(formik.errors.phone)}
-                                />
-                            </div>
-
-                            <label htmlFor="country">Country</label>
-                            <Select
-                                
-                                className={formik.touched.country && formik.errors.country ? styles.error : styles.selectt}
-                                id="country"
-                                name="country"
-                                value={formik.values.country}
-                                placeholder='Select Country'
-                                // defaultValue={}
-                                onChange={value => formik.setFieldValue('country', value)}
-                                onBlur={formik.handleBlur}
-                            >
-                                {countryOptions.map(option => (
-                                    <Select.Option key={option.country} value={option.country}>
-                                        {option.country}
-                                    </Select.Option>
-                                ))}
-                            </Select>
+                            {formOptions.map((field) => (
+                                <React.Fragment key={field.name}>
+                                    <label htmlFor={field.name}>{field.label}</label>
+                                    {renderField(field)}
+                                </React.Fragment>
+                            ))}
                             <span>
                                 By filling out the form, you consent to the processing of personal data
                             </span>
@@ -176,7 +161,67 @@ const Form = () => {
                     </div>
                 </div>
             </div>
+        </div>
+    );
+};
 
+const formOptions = [
+    {
+        label: "Name",
+        name: "name",
+        placeholder: "Pietro Schirano",
+        component: TextField
+    },
+    {
+        label: "Email",
+        name: "email",
+        placeholder: "example@qmeter.net",
+        component: TextField
+    },
+    {
+        label: "Phone Number",
+        name: "phone",
+        placeholder: "(__) ___-__-__",
+        component: TextField,
+    },
+    {
+        label: "Country",
+        name: "country",
+        placeholder: "Select Country",
+        component: Select,
+    }
+];
+
+const CountryPhone = ({ formik, value, phonechange }) => {
+    return (
+        <div
+            className={formik.touched.phone && formik.errors.phone ? styles.error : styles.phoneNumber}
+        >
+            <Select
+                className={styles.phoneSelect}
+                value={value}
+                onChange={value => formik.setFieldValue('num', value)}
+                style={{ width: '140px' }}
+                options={countryOptions.map(option => ({
+                    value: option.code,
+                    label: (
+                        <div className={styles.optionContent}>
+                            <img src={option.flag} alt={option.country} />
+                            <p>{option.code}</p>
+                        </div>
+                    ),
+                }))}
+            />
+            <TextField
+                placeholder='(__) ___-__-__'
+                fullWidth
+                id="phone"
+                name="phone"
+                value={formik.values.phone}
+                onChange={phonechange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.phone && Boolean(formik.errors.phone)}
+            />
         </div>
     );
 };
