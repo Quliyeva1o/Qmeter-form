e45ea59ab66b61d110c1805f6852ba6e57146e8f
Quliyeva1o <renaquliyeva646@gmail.com>
2024-08-01 11:00:26 +0400

    .

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -17,19 +17,9 @@ import tr from '../assets/request/turkey.png';
 import uae from '../assets/request/uae.png';
 import us from '../assets/request/us.png';
 import uss from '../assets/us.png';
+import countryOptions from '../utils/constants.js';
 
-const countryOptions = [
-    { code: "+90", country: "Turkey", flag: "https://www.countryflags.com/wp-content/uploads/turkey-flag-png-large.png", bgi: tr },
-    { code: "+994", country: "Azerbaijan", flag: "https://www.countryflags.com/wp-content/uploads/flag-jpg-xl-11-2048x1024.jpg", bgi: aze },
-    { code: "+962", country: "Jordan", flag: "https://www.countryflags.com/wp-content/uploads/jordan-flag-png-large.png", bgi: jor },
-    { code: "+7", country: "Kazakhstan", flag: "https://www.countryflags.com/wp-content/uploads/kazakhstan-flag-png-large.png", bgi: kaz },
-    { code: "+965", country: "Kuwait", flag: "https://www.countryflags.com/wp-content/uploads/kuwait-flag-png-large.png", bgi: kwt },
-    { code: "+968", country: "Oman", flag: "https://www.countryflags.com/wp-content/uploads/oman-flag-png-large.png", bgi: om },
-    { code: "+48", country: "Poland", flag: "https://www.countryflags.com/wp-content/uploads/poland-flag-png-large.png", bgi: pol },
-    { code: "+966", country: "Saudi Arabia", flag: "https://www.countryflags.com/wp-content/uploads/saudi-arabia-flag-png-large.png", bgi: sa },
-    { code: "+971", country: "United Arab Emirates", flag: "https://www.countryflags.com/wp-content/uploads/united-arab-emirates-flag-png-large.png", bgi: uae },
-    { code: "+1", country: "United States", flag: "https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large.png", bgi: us },
-];
+
 
 const LocalCountries = {
     AZ: aze,
